9958804c7142df7811d26f6fadde0e07bc430d96
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-11-03 21:53:51 +0545

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,7 @@ function MyApp({ Component, pageProps }) {
 
       <Head>
         <meta name='asg_verification' content='vVcWCcbbgmnqv221hpAjPojb' />
+        <meta name="Trafficstars" content="48726"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
