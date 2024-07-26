719e0c9df8919e3fe6ffe1f0bbf02b9bcb2b3c2c
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-07-27 02:00:58 +0545

    sadfsda

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    BACKEND_URL: 'https://clownfish-app-jn7w9.ondigitalocean.app/desikahaniya/',   
-    // BACKEND_URL: 'http://localhost:5000/desikahaniya/', 
-   FRONTEND_URL: 'http://localhost:3000/', 
-    //  FRONTEND_URL: 'http://Hindisexstory.app/',
+    // BACKEND_URL: 'https://cronjob-yr9s.onrender.com/', 
+    // BACKEND_URL: 'https://clownfish-app-jn7w9.ondigitalocean.app/desikahaniya/',   
+    BACKEND_URL: 'http://localhost:5000/desikahaniya/', 
+   // FRONTEND_URL: 'http://localhost:3000/', 
+     FRONTEND_URL: 'http://Hindisexstory.app/',
     FACEBOOK_APP_SECRET:'4c30ed043dbfc9e5b59e9db484283609',
     FACEBOOK_APP_ID:'1762910734100962',
     GOOGLE_CLIENT_ID:'737624608726-qa70ffm7anrn03v0qu8pgoed8bcfsfk5.apps.googleusercontent.com',
