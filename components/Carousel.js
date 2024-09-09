687ab9469742bde7cecb11a3109b212c13a73323
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-09-09 07:34:49 +0545

    asdf

--- components/Carousel.js
+++ components/Carousel.js
@@ -42,7 +42,7 @@ const Carousel = ({ imageUrls }) => {
     return (
         <div className={`${showCarousel ? "fixed" : "hidden"} inset-0 flex items-center justify-center bg-black bg-opacity-90 select-none`} data-carousel="slide">
             <div className='absolute right-4 top-4 lg:top-8 lg:right-8 p-3 z-50 rounded-full bg-black bg-opacity-50 flex justify-center items-center'>
-                <IoIosCloseCircleOutline onClick={() => { setshowCarausel(false) }} className="cursor-pointer  text-white text-[30px] lg:text-[40px] " />
+                <IoIosCloseCircleOutline onClick={() => { setshowCarausel(false) }} className="cursor-pointer  text-gray-300 text-[30px] lg:text-[40px] " />
             </div>
 
             {/* Carousel wrapper */}
