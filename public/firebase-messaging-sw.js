a2e9f55f24a1c87505adcc8ceeb473ba8fb67e52
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-09-13 02:01:30 +0545

    sdafdsa

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-importScripts(  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
 // Replace these with your own Firebase config keys...
 const firebaseConfig = {
@@ -22,12 +22,10 @@ messaging.onBackgroundMessage((payload) => {
         payload
     );
 
-    console.log(payload);
-    
 
-    // payload.fcmOptions?.link comes from our backend API route handle
+
     // payload.data.link comes from the Firebase Console where link is the 'key'
-    const link = payload.fcmOptions?.link || payload.data?.link;
+    const link = payload.data?.link;
 
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
@@ -53,36 +51,3 @@ self.addEventListener('notificationclick', (event) => {
 
 
 
-
-
-
-
-// self.addEventListener("notificationclick", function (event) {
-//     console.log("[firebase-messaging-sw.js] Notification click received.");
-
-//     event.notification.close();
-
-//     // This checks if the client is already open and if it is, it focuses on the tab. If it is not open, it opens a new tab with the URL passed in the notification payload
-//     event.waitUntil(
-//         clients
-//             // https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
-//             .matchAll({ type: "window", includeUncontrolled: true })
-//             .then(function (clientList) {
-//                 const url = event.notification.data.url;
-
-//                 if (!url) return;
-
-//                 // If relative URL is passed in firebase console or API route handler, it may open a new window as the client.url is the full URL i.e. https://example.com/ and the url is /about whereas if we passed in the full URL, it will focus on the existing tab i.e. https://example.com/about
-//                 for (const client of clientList) {
-//                     if (client.url === url && "focus" in client) {
-//                         return client.focus();
-//                     }
-//                 }
-
-//                 if (clients.openWindow) {
-//                     console.log("OPENWINDOW ON CLIENT");
-//                     return clients.openWindow(url);
-//                 }
-//             })
-//     );
-// });
